577a5df24f2186683ef5de765651db209f26624c
meryhotma <merysitumorang.business@gmail.com>
2026-02-17 01:16:30 -0500

    Add Streamlit app for Part 2

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+# import necessary libraries
+import pandas as pd
+import plotly.express as px
+import requests
+import streamlit as st
+
+# set title of the app
+
+st.title("NYC Building Insights-Job Application")
