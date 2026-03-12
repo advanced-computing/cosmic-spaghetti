aebd8babed997ab27b2ba0ac3092fe268c1b1dc9
meryhotma <merysitumorang.business@gmail.com>
2026-03-12 10:09:10 -0400

    project part 4 and bug fixing

--- functions/permit_validation.py
+++ functions/permit_validation.py
@@ -2,13 +2,15 @@ from pandera import Column, DataFrameSchema
 
 permit_schema = DataFrameSchema(
     {
-        "borough": Column(object, nullable=True),
-        "issued_date": Column(object, nullable=True),
-        "approved_date": Column(object, nullable=True),
-        "expired_date": Column(object, nullable=True),
-        "work_type": Column(object, nullable=True),
-        "permit_status": Column(object, nullable=True),
-        "community_board": Column(object, nullable=True),
+        "borough": Column(object, nullable=True, required=False),
+        "issued_date": Column(object, nullable=True, required=False),
+        "approved_date": Column(object, nullable=True, required=False),
+        "expired_date": Column(object, nullable=True, required=False),
+        "work_type": Column(object, nullable=True, required=False),
+        "permit_status": Column(object, nullable=True, required=False),
+        "community_board": Column(object, nullable=True, required=False),
     },
     coerce=False,
+    # allow extra or missing columns
+    strict=False,
 )
